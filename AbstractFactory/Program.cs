d54a987c6bc9f0d3807d94d36f133505a22919c2
Anna Yosha <yoshaanna1511@gmail.com>
2025-12-05 01:04:39 +0200

    Добавьте файлы проекта.

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    interface ICarFactory
+    {
+        Car CreateCar();
+        Engine CreateEngine();
+    }
+
+    abstract class Car
+    {
+        public abstract void Info();
+    }
+
+    abstract class Engine
+    {
+        public abstract void GetPower();
+    }
+
+    class Ford : Car
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Ford");
+        }
+    }
+
+    class Toyota : Car
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Toyota");
+        }
+    }
+
+    class Mercedes : Car
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Mercedes");
+        }
+    }
+
+    class FordEngine : Engine
+    {
+        public override void GetPower()
+        {
+            Console.WriteLine("Ford Engine 4.4");
+        }
+    }
+
+    class ToyotaEngine : Engine
+    {
+        public override void GetPower()
+        {
+            Console.WriteLine("Toyota Engine 3.2");
+        }
+    }
+
+    class MercedesEngine : Engine
+    {
+        public override void GetPower()
+        {
+            Console.WriteLine("Mercedes Engine 5.0");
+        }
+    }
+
+    class FordFactory : ICarFactory
+    {
+        public Car CreateCar()
+        {
+            return new Ford();
+        }
+
+        public Engine CreateEngine()
+        {
+            return new FordEngine();
+        }
+    }
+
+    class ToyotaFactory : ICarFactory
+    {
+        public Car CreateCar()
+        {
+            return new Toyota();
+        }
+
+        public Engine CreateEngine()
+        {
+            return new ToyotaEngine();
+        }
+    }
+
+    class MercedesFactory : ICarFactory
+    {
+        public Car CreateCar()
+        {
+            return new Mercedes();
+        }
+
+        public Engine CreateEngine()
+        {
+            return new MercedesEngine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Toyota ---");
+            ICarFactory carFactory = new ToyotaFactory();
+            Car myCar = carFactory.CreateCar();
+            myCar.Info();
+            Engine myEngine = carFactory.CreateEngine();
+            myEngine.GetPower();
+
+            Console.WriteLine("\n--- Ford ---");
+            carFactory = new FordFactory();
+            myCar = carFactory.CreateCar();
+            myCar.Info();
+            myEngine = carFactory.CreateEngine();
+            myEngine.GetPower();
+
+            Console.WriteLine("\n--- Mercedes ---");
+            carFactory = new MercedesFactory();
+            myCar = carFactory.CreateCar();
+            myCar.Info();
+            myEngine = carFactory.CreateEngine();
+            myEngine.GetPower();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorExample
+{
+    abstract class ChristmasTree
+    {
+        public abstract string GetDescription();
+        public abstract void Shine();
+    }
+
+    class RealChristmasTree : ChristmasTree
+    {
+        public override string GetDescription()
+        {
+            return "Simple Fir Tree";
+        }
+
+        public override void Shine()
+        {
+            Console.WriteLine("The tree does not shine without garlands.");
+        }
+    }
+
+    abstract class TreeDecorator : ChristmasTree
+    {
+        protected ChristmasTree tree;
+
+        public TreeDecorator(ChristmasTree tree)
+        {
+            this.tree = tree;
+        }
+
+        public override string GetDescription()
+        {
+            return tree.GetDescription();
+        }
+
+        public override void Shine()
+        {
+            tree.Shine();
+        }
+    }
+
+    class OrnamentDecorator : TreeDecorator
+    {
+        private string ornamentType;
+
+        public OrnamentDecorator(ChristmasTree tree, string type)
+            : base(tree)
+        {
+            this.ornamentType = type;
+        }
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() + " with " + ornamentType;
+        }
+
+        public override void Shine()
+        {
+            base.Shine();
+        }
+    }
+
+    class GarlandDecorator : TreeDecorator
+    {
+        private int lightCount;
+
+        public GarlandDecorator(ChristmasTree tree, int count)
+            : base(tree)
+        {
+            this.lightCount = count;
+        }
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() + " and bright garlands (" + lightCount + " lights)";
+        }
+
+        public override void Shine()
+        {
+            base.Shine();
+            Console.WriteLine("The garlands are shining brightly!");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ChristmasTree myTree = new RealChristmasTree();
+            Console.WriteLine("Base Tree: " + myTree.GetDescription());
+            myTree.Shine();
+            Console.WriteLine(new string('-', 20));
+
+            ChristmasTree decoratedTree = new OrnamentDecorator(myTree, "red balls");
+            decoratedTree = new GarlandDecorator(decoratedTree, 50);
+            decoratedTree = new OrnamentDecorator(decoratedTree, "stars");
+
+            Console.WriteLine("Decorated Tree: " + decoratedTree.GetDescription());
+            decoratedTree.Shine();
+
+            Console.ReadKey();
+        }
+    }
+}
